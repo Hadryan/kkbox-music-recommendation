45b1feb0a5f6010228906c7abb5b8046244ce9ec
Zhengyang Song <songzy_thu@163.com>
2021-02-02 23:24:43 +0800

    solution 1 feature extractor

---
+++ solution/Codes-for-WSDM-CUP-Music-Rec-1st-place-solution/feature_extractor/run.sh
@@ -0,0 +1,8 @@
+python3 id_process.py &&\
+python3 cnt_log_process.py &&\
+python3 isrc_process.py &&\
+python3 svd_process.py &&\
+python3 timestamp_process.py &&\
+python3 before_after_process.py &&\
+python3 data_for_training.py &&\
+python3 experiments.py
